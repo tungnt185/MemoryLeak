86b2334a2ec5bdd71b647f9ed4d3dfb5cae35d5f
nttung <misaonly2013@gmail.com>
2014-08-28 18:28:52 +0700

    Add

---
+++ MemoryLeak/MemoryLeak/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MemoryLeak
+{
+    public partial class Form1 : Form
+    {
+        public event Action SomeStaticEvent;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Form2 frm = new Form2();
+            Form2 frm = new Form2(this);
+            frm.ShowDialog();
+        }
+    }
+
+    public static class StaticStuff
+    {
+        public static event Action SomeStaticEvent;
+    }
+
+}
---
+++ MemoryLeak/MemoryLeak/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MemoryLeak
+{
+    public partial class Form2 : Form
+    {
+        private event Action SomeStaticEvent;
+
+        public Form2()
+        {
+            InitializeComponent();
+            SomeStaticEvent += DoSomething;
+        }
+
+        Form1 form1;
+
+        public Form2(Form1 frm)
+        {
+            InitializeComponent();
+            form1 = new Form1();
+            form1.SomeStaticEvent += DoSomething;
+        }
+
+        private void DoSomething()
+        {
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            //form1.SomeStaticEvent -= DoSomething;
+            base.OnClosed(e);
+        }
+    }
+}
